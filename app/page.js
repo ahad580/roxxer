8fe1e3302d4d864fde83d3a5d0578447b1471514
ahadx16 <125337827+ahad580@users.noreply.github.com>
2025-12-31 16:11:01 -0800

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,5 @@
-import Image from "next/image";
-import styles from "./page.module.css";
-import Link from "next/link";
+import { redirect } from "next/navigation";
+
 export default function Home() {
-  return (
-    <>
-    </>
-  );
+  redirect("/about");
 }
